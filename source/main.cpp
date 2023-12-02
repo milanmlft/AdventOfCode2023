63a643583712ef52274a72419be75c9ced18b738
Milan Malfait <m.malfait@ucl.ac.uk>
2023-12-02 23:09:51 +0000

    Add get_digits()

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,15 @@
+#include <cctype>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
+using std::cout;
+using std::endl;
 using std::string;
+using std::vector;
 
-void printProcessedFile(std::string fileName) {
+void printProcessedFile(string fileName) {
     std::ifstream infile(fileName);
 
     string line;
@@ -13,13 +18,22 @@ void printProcessedFile(std::string fileName) {
     }
 }
 
-int main() {
-    try {
-        printProcessedFile("data/day1-input.txt");
-    } catch (std::exception &e) {
-        std::cout << "Exception caught: " << e.what() << std::endl;
-        std::cout << "We no longer have access to the file handle." << std::endl;
+vector<int> get_digits(string line) {
+    vector<int> result;
+    for (auto &c : line) {
+        if (std::isdigit(c)) {
+            result.push_back(int(c) - int('0'));
+        }
     }
+    return result;
+}
 
+int main() {
+    vector<int> digits = get_digits("a1b2c3d4e5");
+    for (auto &d : digits) {
+        cout << d;
+    }
+    cout << endl;
+    cout << int('1') - int('0') << endl;
     return 0;
 }
