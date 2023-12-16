9a59b5d958eec30b33f580d86da2e0fe20d88766
Milan Malfait <m.malfait@ucl.ac.uk>
2023-12-16 10:57:28 +0000

    Day 1B solution

--- source/main.cpp
+++ source/main.cpp
@@ -1,34 +1,106 @@
+#include <algorithm>
 #include <cctype>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
-#include <vector>
 
 using std::cout;
 using std::endl;
+using std::map;
+using std::size_t;
 using std::string;
-using std::vector;
+
+// Create map of string digits to the corresponding integers
+map<string, int> digits_map{
+    {"one", 1}, {"two", 2},   {"three", 3}, {"four", 4}, {"five", 5},
+    {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9},
+};
+
+void print_file(const string &filename) {
+    std::ifstream infile(filename);
+    string line;
+    while (std::getline(infile, line)) {
+        cout << line << endl;
+    }
+}
 
 int convert_char_to_int(const char &c) { return int(c) - int('0'); }
 
-int get_calibration_value(const string &line) {
-int get_calibration_value(string &line) {
-    vector<int> digits;
+// Replace instances of string digits with their integer counterparts
+void replace_first_string_digit(string &line) {
+    std::size_t smallest_pos = line.length();
+    string digit_to_replace;
 
-    string::iterator it;
-    for (it = line.begin(); it < line.end(); it++) {
-        if (std::isdigit(*it)) {
-            digits.push_back(convert_char_to_int(*it));
+    for (map<string, int>::iterator it = digits_map.begin(); it != digits_map.end(); ++it) {
+        string cur_digit = it->first;
+        size_t digit_pos = line.find(cur_digit);
+        if (digit_pos < smallest_pos) {
+            smallest_pos = digit_pos;
+            digit_to_replace = cur_digit;
+        }
+    }
+    if (smallest_pos < line.length()) {
+        line.replace(smallest_pos, digit_to_replace.length(),
+                     std::to_string(digits_map[digit_to_replace]));
+    }
+}
+
+void replace_last_string_digit(string &line) {
+    std::size_t largest_pos = 0;
+    string digit_to_replace;
+
+    for (map<string, int>::iterator it = digits_map.begin(); it != digits_map.end(); ++it) {
+        string cur_digit = it->first;
+        size_t digit_pos = line.find(cur_digit);
+        if (digit_pos > largest_pos && digit_pos < line.length()) {
+            largest_pos = digit_pos;
+            digit_to_replace = cur_digit;
         }
     }
 
+    // If largest_pos = 0, this should be handled by replace_first_string_digit()
+    if (largest_pos > 0) {
+        line.replace(largest_pos, digit_to_replace.length(),
+                     std::to_string(digits_map[digit_to_replace]));
+    }
+}
+
+int get_first_digit(const string &line) {
+    string line_copy{line};
+    replace_first_string_digit(line_copy);
+
+    // Return first digit encountered
+    for (auto &c : line_copy) {
+        if (std::isdigit(c)) {
+            return (convert_char_to_int(c));
+        }
+    }
+    throw std::runtime_error("No digit found in the line.");
+}
+
+int get_last_digit(const string &line) {
+    string line_copy{line};
+    replace_last_string_digit(line_copy);
+    std::reverse(line_copy.begin(), line_copy.end());
+
+    // Return first digit encountered
+    for (auto &c : line_copy) {
+        if (std::isdigit(c)) {
+            return (convert_char_to_int(c));
+        }
+    }
+    throw std::runtime_error("No digit found in the line.");
+}
+
+int get_calibration_value(string &line) {
     // Construct 2-digit number from first and last digits
-    return 10 * digits[0] + digits[digits.size() - 1];
+    int result = 10 * get_first_digit(line) + get_last_digit(line);
+    return result;
 }
 
 int main() {
     string input_file = "data/day1-input.txt";
-
     std::ifstream infile(input_file);
 
     int result = 0;
@@ -39,6 +111,5 @@ int main() {
     }
 
     cout << "result = " << result << endl;
-
     return 0;
 }
