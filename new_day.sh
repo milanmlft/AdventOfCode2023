861dcee685ef81ce20211ff693f5556607dac8c9
Milan Malfait <m.malfait@ucl.ac.uk>
2023-12-18 17:28:13 +0000

    Script to create folder structure for new day

---
+++ new_day.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+NEW_DIR="day${1}"
+
+mkdir -p "${NEW_DIR}/source"
+touch ${NEW_DIR}/source/main.cpp
+
+cp day1/CMakeLists.txt ${NEW_DIR}/
+cp day1/source/CMakeLists.txt ${NEW_DIR}/source/
