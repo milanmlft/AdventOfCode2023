e7291c057d38b62e1c09ee115012ae2dd28579dd
Milan Malfait <m.malfait@ucl.ac.uk>
2023-12-19 20:30:08 +0000

    Also create data directory for new days

--- new_day.sh
+++ new_day.sh
@@ -7,5 +7,7 @@ NEW_DIR="day${1}"
 mkdir -p "${NEW_DIR}/source"
 touch ${NEW_DIR}/source/main.cpp
 
+mkdir -p "${NEW_DIR}/data"
+
 cp day1/CMakeLists.txt ${NEW_DIR}/
 cp day1/source/CMakeLists.txt ${NEW_DIR}/source/
